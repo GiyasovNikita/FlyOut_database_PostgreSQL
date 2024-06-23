3f49f174cd282eaa080ab2d61bd9caa7516428e4
GiyasovNikita <129554012+GiyasovNikita@users.noreply.github.com>
2024-06-23 05:45:22 +0500

    refactor, added tables city and pivot table

--- flyout_db_tables_init.sql
+++ flyout_db_tables_init.sql
@@ -1,32 +1,48 @@
-/*Создание таблицы Airports*/
-CREATE TABLE Airports (
-    airport_code CHAR(3) PRIMARY KEY,
+/*Создание таблицы airport*/
+CREATE TABLE airport (
+	airport_id SERIAL PRIMARY KEY,
+    airport_code CHAR(3),
     airport_name VARCHAR(255) NOT NULL
 );
 
+/*Создание таблицы city*/
+create table city (
+	city_id SERIAL PRIMARY KEY,
+	city_name varchar(15)
+);
 
-/*Создание таблицы Airlines*/
-CREATE TABLE Airlines (
-    airline_code CHAR(2) PRIMARY KEY,
+/*Создание таблицы airport_city*/
+CREATE TABLE airport_city (
+	airport_city_id SERIAL PRIMARY KEY,
+    airport_id INT,
+    city_id INT,
+    FOREIGN KEY (airport_id) REFERENCES airport(airport_id),
+    FOREIGN KEY (city_id) REFERENCES city(city_id)
+);
+
+/*Создание таблицы airline*/
+CREATE TABLE airline (
+	airline_id SERIAL PRIMARY KEY,
+    airline_code CHAR(2),
     airline_name VARCHAR(255) NOT NULL
 );
 
-/*Создание таблицы Flights*/
-CREATE TABLE Flights (
+/*Создание таблицы flight*/
+CREATE TABLE flight (
     flight_id SERIAL PRIMARY KEY,
-    origin_airport CHAR(3) REFERENCES Airports(airport_code),
-    destination_airport CHAR(3) REFERENCES Airports(airport_code),
-    airline_code CHAR(2) REFERENCES Airlines(airline_code),
+    origin_airport INT REFERENCES airport_city(airport_city_id),
+    destination_airport INT REFERENCES airport_city(airport_city_id),
+    airline_id INT REFERENCES airline(airline_id),
     flight_number VARCHAR(10),
     departure_at TIMESTAMP,
     return_at TIMESTAMP,
     duration INT
 );
 
-/*Создание таблицы FlightDetails*/
-CREATE TABLE FlightDetails (
-    flight_detail_id SERIAL PRIMARY KEY,
-    flight_id INT REFERENCES Flights(flight_id),
+/*Создание таблицы flight_details*/
+CREATE TABLE flight_details (
+    flight_details_id SERIAL PRIMARY KEY,
+    flight_id INT REFERENCES flight(flight_id),
     price NUMERIC(10, 2),
     currency CHAR(3),
     transfers INT,
